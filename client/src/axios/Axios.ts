abf150acbb77d566bfcd47fc7ea0787beed25dfc
sufiyanbmk <sufiyanbmk@gmail.com>
2023-07-25 09:32:26 +0530

    baseurl

--- client/src/axios/Axios.ts
+++ client/src/axios/Axios.ts
@@ -1,6 +1,6 @@
 import axios from 'axios'
 
-const baseURL = 'http://localhost:8080/'
+const baseURL = 'https://online-notes-server.onrender.com/'
 
 const axiosInstance=  axios.create({
     baseURL,
